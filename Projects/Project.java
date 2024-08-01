e41aff1a7adff0cbd99bba0f982d3f08269114f8
RmFelipe24 <rimunozro@est.utn.ac.cr>
2024-08-01 16:43:18 -0600

    Clase Project
    Se crea la clase

---
+++ Projects/Project.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Projects;
+
+import Comentarios.Coments;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author munoz
+ */
+public class Project {
+    private int id;
+    private String name;
+    private ArrayList<Coments> ListTasks;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Project(ArrayList<Coments> ListTasks) {
+        this.ListTasks = ListTasks;
+    }
+
+   
+   public void AddTask(){
+   
+   
+   } 
+
+    public Project(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.ListTasks = ListTasks;
+    }
+
+    public Project() {
+        
+    }
+    
+   
+   
+    
+}

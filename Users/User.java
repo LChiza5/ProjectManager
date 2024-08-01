5050b714caecc1b4598f826ce8974fe44ad0b12c
chiza <luisdanielav05@gmail.com>
2024-08-01 15:36:06 -0600

    Clase User
    Se crea la clase User

--- Users/User.java
+++ Users/User.java
@@ -9,5 +9,32 @@ package Users;
  * @author Luisk
  */
 public class User {
-    
+    private String id;
+    private String name;
+    private String email; 
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
 }
